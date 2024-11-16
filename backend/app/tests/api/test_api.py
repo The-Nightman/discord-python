96facd61d5dc538c6b5c21302144a532bbc87df3
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-11-16 15:12:00 +0000

    add test to check that superuser is created

--- backend/app/tests/api/test_api.py
+++ backend/app/tests/api/test_api.py
@@ -1,5 +1,8 @@
 from fastapi.testclient import TestClient
+from sqlmodel import Session, select
 from app.main import app
+from app.core.config import settings
+from app.models import User
 
 client = TestClient(app)
 
@@ -8,8 +11,18 @@ def test_pytest_function():
     assert bool("true") == True
 
 
-# ! Currently commented out due to possible PYTHONPATH issues?
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_initial_super_user_is_created(db: Session):
+    # Query the database for the superuser
+    user = db.exec(select(User).where(User.email == settings.FIRST_SUPERUSER)).first()
+
+    # Check if the superuser exists
+    assert user is not None
+    assert user.email == settings.FIRST_SUPERUSER
+    assert user.username == settings.FIRST_SUPERUSER_USERNAME
+    assert user.is_super_admin == True
