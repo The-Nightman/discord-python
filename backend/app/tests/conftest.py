055f77ae6752c98af5b6900cfeecd44992621f8b
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-11-16 18:06:13 +0000

    move testclient into conftest.py

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from collections.abc import Generator
+from fastapi.testclient import TestClient
+from app.main import app
 from app.core.logger import logger
 from app.core.config import settings
 from app.core.db import init_db
@@ -7,6 +9,7 @@ from app.models import User, Server, UserServerLink, Message
 from sqlmodel import Session, create_engine, delete, SQLModel
 from sqlalchemy_utils import database_exists, create_database
 
+client = TestClient(app)
 
 # Create a test database
 test_engine = create_engine(str(settings.TEST_SQLALCHEMY_DATABASE_URI()))
@@ -35,3 +38,9 @@ def db() -> Generator[Session, None, None]:
         statement = delete(Message)
         session.exec(statement)
         session.commit()
+
+
+@pytest.fixture(scope="session")
+def client() -> Generator[TestClient, None, None]:
+    with TestClient(app) as c:
+        yield c
