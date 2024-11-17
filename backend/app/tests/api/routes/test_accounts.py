8ebed22e8c8d05d2fc7b7a72c07fa684075c01a6
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-11-17 17:14:05 +0000

    add register new user and register used email tests

--- backend/app/tests/api/routes/test_accounts.py
+++ backend/app/tests/api/routes/test_accounts.py
@@ -1,18 +1,18 @@
 from fastapi.testclient import TestClient
-from sqlmodel import Session
 from app.core.config import settings
 
 
-def test_login_recieves_token(client: TestClient, db: Session):
+def test_login_recieves_token(client: TestClient):
     login_data = {
         "username": settings.FIRST_SUPERUSER,
         "password": settings.FIRST_SUPERUSER_PASSWORD,
     }
-    response = client.post("/api/v1/accounts/login", data=login_data)
+    response = client.post(
+        f"{settings.API_V1_STR}/accounts/login", data=login_data)
     token = response.json()
     assert response.status_code == 200
-    assert "access_token" in token # Check if the token key is present
-    assert token["access_token"] # Check if the token is not empty
+    assert "access_token" in token  # Check if the token key is present
+    assert token["access_token"]  # Check if the token is not empty
 
 
 def test_login_with_wrong_password(client: TestClient):
@@ -20,7 +20,48 @@ def test_login_with_wrong_password(client: TestClient):
         "username": settings.FIRST_SUPERUSER,
         "password": "wrong_password",
     }
-    response = client.post("/api/v1/accounts/login", data=login_data)
+    response = client.post(
+        f"{settings.API_V1_STR}/accounts/login", data=login_data)
     assert response.status_code == 401
     assert response.json() == {"detail": "Incorrect email or password"}
-    assert "access_token" not in response.json() # Check if the token key is not present
+    # Check if the token key is not present
+    assert "access_token" not in response.json()
+
+
+def test_register_new_user(client: TestClient):
+    user_in = {
+        "username": "testuser",
+        "email": "testuser@mail.com",
+        "password": "testpassword",
+    }
+    response = client.post(
+        f"{settings.API_V1_STR}/accounts/register", json=user_in)
+    token = response.json()
+    assert response.status_code == 201
+    assert "access_token" in token
+    assert token["access_token"]
+
+
+def test_register_used_email(client: TestClient):
+    initial_user = {
+        "username": "testuser",
+        "email": "testuser@mail.com",
+        "password": "testpassword",
+    }
+    user_in = {
+        "username": "seconduser",
+        "email": "testuser@mail.com",
+        "password": "secrets!",
+    }
+
+    client.post(f"{settings.API_V1_STR}/accounts/register",
+                json=initial_user)  # Register the initial user
+
+    # Try to register the second user with the same email
+    response = client.post(
+        f"{settings.API_V1_STR}/accounts/register", json=user_in)
+
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Email already registered"}
+    # Check if the token key is not present
+    assert "access_token" not in response.json()
