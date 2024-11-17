ab835db7616e1aebc63c2fa8359a14f65b6dda72
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-11-17 23:15:16 +0000

    add register route to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { UnauthenticatedLayout } from "./layouts/UnauthenticatedLayout";
 import { Login } from "./routes/unauthenticated/Login";
+import { Register } from "./routes/unauthenticated/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/" Component={UnauthenticatedLayout}>
         <Route path="/" Component={Login} />
+        <Route path="/register" Component={Register} />
       </Route>
     </Routes>
   );
