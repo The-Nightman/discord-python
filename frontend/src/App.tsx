f250aaeb1ec0d0f35f1bb47c0b76054e1587151c
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-11-17 02:03:30 +0000

    add Login route path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { UnauthenticatedLayout } from "./layouts/UnauthenticatedLayout";
+import { Login } from "./routes/unauthenticated/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" Component={UnauthenticatedLayout}>
+        <Route path="/" Component={Login} />
       </Route>
     </Routes>
   );
