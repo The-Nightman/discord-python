e4a2e96c42dae27386b2354fba53bd35fa60e5bc
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-11-17 18:50:09 +0000

    add users controller to router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.routes import accounts
+from app.api.routes import accounts, users
 
 api_router = APIRouter()
 api_router.include_router(accounts.router, prefix="/accounts", tags=["accounts"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
