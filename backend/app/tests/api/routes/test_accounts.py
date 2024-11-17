3b27fa6a4c4e16222cd073d58596ad0367faa53e
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-11-17 15:47:45 +0000

    add missing test for incorrect password

--- backend/app/tests/api/routes/test_accounts.py
+++ backend/app/tests/api/routes/test_accounts.py
@@ -13,3 +13,14 @@ def test_login_recieves_token(client: TestClient, db: Session):
     assert response.status_code == 200
     assert "access_token" in token # Check if the token key is present
     assert token["access_token"] # Check if the token is not empty
+
+
+def test_login_with_wrong_password(client: TestClient):
+    login_data = {
+        "username": settings.FIRST_SUPERUSER,
+        "password": "wrong_password",
+    }
+    response = client.post("/api/v1/accounts/login", data=login_data)
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Incorrect email or password"}
+    assert "access_token" not in response.json() # Check if the token key is not present
