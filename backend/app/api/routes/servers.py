a33ae22bc32a102b1bae7e28d7ecb9272e0d57da
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-11-19 17:26:29 +0000

    add get_server endpoint to servers controller to fetch server details by id

--- backend/app/api/routes/servers.py
+++ backend/app/api/routes/servers.py
@@ -1,12 +1,32 @@
 from fastapi import APIRouter, status, HTTPException
 from typing import Any
-from sqlmodel import select
 from sqlalchemy.exc import SQLAlchemyError
 from app.api.deps import SessionDep, CurrentUser
-from app.models import ServerCreate, Server, ServerUpdate
+from app.models import ServerCreate, Server, ServerUpdate, ServerPublic
 from app import crud
 
 router = APIRouter()
+
+
+@router.get("/{server_id}", response_model=ServerPublic, status_code=status.HTTP_200_OK)
+async def get_server(session: SessionDep, current_user: CurrentUser, server_id: str) -> Any:
+    """
+    Get a server by ID
+    """
+    try:
+        server = crud.get_server_by_id(session=session, server_id=server_id)
+        if not server:
+            raise HTTPException(
+                status_code=404, detail="The server with this ID does not exist.")
+        return server
+    except SQLAlchemyError as e:
+        raise HTTPException(
+            status_code=500, detail="An unknown database error has occured, if this persists please contact support.")
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=400, detail=f"An error occured: {str(e)}") from e
 
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
@@ -35,16 +55,12 @@ async def update_server(session: SessionDep, current_user: CurrentUser, server_i
         if crud.user_is_owner(session=session, user_id=current_user.id, server_id=server_id) == False:
             raise HTTPException(
                 status_code=403, detail="You do not have permission to update this server.")
-
         server = crud.update_server_name(
             session=session, server_id=server_id, name_in=new_name.name)
-
         if not server:
             raise HTTPException(
                 status_code=404, detail="The server with this ID does not exist.")
-
         return server
-
     except SQLAlchemyError as e:
         raise HTTPException(
             status_code=500, detail="An unknown database error has occured, if this persists please contact support.")
